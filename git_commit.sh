f3530a50687b283c76031a9f3b2d85768d5a9689
havecourage <g610413499@gmail.com>
2020-09-17 19:00:10 +0800

    3

--- git_commit.sh
+++ git_commit.sh
@@ -3,5 +3,12 @@
 #向脚本之中加入参数，参数为提交之时所要添加的信息
 git add ./* ./*/*
 git commit -m $1
-git remote add origin https://github.com/havecourage/-.git
+#git remote add origin https://github.com/havecourage/-.git
 git push -u origin master
+
+#    git config --global user.name "Your Name"
+#    git config --global user.email you@example.com
+
+#    After doing this, you may fix the identity used for this commit with:
+
+#    git commit --amend --reset-author
