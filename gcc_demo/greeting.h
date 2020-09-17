832df5e03175b065d2866ceb5e1e47f091f57554
havecourage <g610413499@gmail.com>
2020-09-17 18:58:31 +0800

    3

--- gcc_demo/greeting.h
+++ gcc_demo/greeting.h
@@ -3,3 +3,10 @@ void greeting();
 void hello();
 int system();
 void content();
+
+//    git config --global user.name "Your Name"
+//    git config --global user.email you@example.com
+
+//    After doing this, you may fix the identity used for this commit with:
+
+//    git commit --amend --reset-author
